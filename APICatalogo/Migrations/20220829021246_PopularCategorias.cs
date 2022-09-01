13e14ec9da79a5a2a09d11ba2c2907e0b4be9127
MatheusdeOliveiraCorrea <93480352+MatheusdeOliveiraCorrea@users.noreply.github.com>
2022-09-01 19:46:33 -0300

    populando campos do banco de dados

--- APICatalogo/Migrations/20220829021246_PopularCategorias.cs
+++ APICatalogo/Migrations/20220829021246_PopularCategorias.cs
@@ -2,21 +2,20 @@
 
 #nullable disable
 
-namespace APICatalogo.Migrations
-{
-    public partial class PopularCategorias : Migration
-    {
-        protected override void Up(MigrationBuilder mb)
-        {
-            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Bebidas', 'bebidas.jpg')");
-            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Lanches', 'lanches.jpg')");
-            mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Sobremesas', 'sobremesas.jpg')");
-            
-        }
+namespace APICatalogo.Migrations;
 
-        protected override void Down(MigrationBuilder mb)
-        {
-            mb.Sql("Delete from Categorias");
-        }
+public partial class PopularCategorias : Migration
+{
+    protected override void Up(MigrationBuilder mb)
+    {
+        mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Bebidas', 'bebidas.jpg')");
+        mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Lanches', 'lanches.jpg')");
+        mb.Sql("Insert into Categorias(Nome, ImagemUrl) values('Sobremesas', 'sobremesas.jpg')");
+        
+    }
+
+    protected override void Down(MigrationBuilder mb)
+    {
+        mb.Sql("Delete from Categorias");
     }
 }
