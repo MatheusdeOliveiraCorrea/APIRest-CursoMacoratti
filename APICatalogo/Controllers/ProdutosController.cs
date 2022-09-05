3df3e36fa239138f9034fd0e82b50a3016d7d738
MatheusdeOliveiraCorrea <93480352+MatheusdeOliveiraCorrea@users.noreply.github.com>
2022-09-04 23:30:24 -0300

    aula 21

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,44 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly APICatalogoContext _context;
+
+    public ProdutosController(APICatalogoContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+        if (produtos is null) return NotFound("Produtos não encontrados");
+        return produtos;
+    }
+
+    [HttpGet("{id:int}", Name = "ObterProduto")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null) return NotFound("Produto com ID: " + id + " não encontrado, por favor, tente outro ID.");
+        return produto;
+    }
+
+    [HttpPost]
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null) return BadRequest();
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return new CreatedAtRouteResult("ObterProduto",
+            new { id = produto.ProdutoId }, produto);
+    }
+}
